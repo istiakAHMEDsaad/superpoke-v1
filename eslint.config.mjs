031c2b519b4ba07f926aaf1ff485d0bd4cf7e90b
istiakAHMEDsaad <istik2020@gmail.com>
2025-12-28 14:57:03 +0600

    project setup

---
+++ eslint.config.mjs
@@ -0,0 +1,44 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import tsParser from '@typescript-eslint/parser';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      '@next/next/no-img-element': 'error',
+    },
+  },
+  {
+    ignores: [
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
+    ],
+  },
+];
+
+export default eslintConfig;
---
+++ src/components/ImageCon.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ImageCon = () => {
+  return (
+    <div>
+      <h1>ImageCon</h1>
+    </div>
+  );
+};
+
+export default ImageCon;
